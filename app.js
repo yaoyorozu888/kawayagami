e8f176293da01cf157d079541c0c78139c75eaa7
yaoyorozu888 <seeeeeeeji@gmail.com>
2020-11-30 02:15:09 +0900

    2020/11/29日文作業。node.js勉強

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const http = require('http');
-const fs=require("fs");
+const fs = require("fs");
+const ejs = require("ejs");
+const url = require("url");
 
-var server = http.createServer(
-    (request, response) => {
-        fs.readFile('./index.html', 'UTF-8',
-        (error, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(data);
-            response.end();
-        });
-    }
-);
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
+const style_css = fs.readFileSync('./style.css', 'utf8');
+
+
+var server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start!');
+
+// ここまでメインプログラム
+
+//  createServerの処理
+function getFromClient(request, response) {
+    var url_parts = url.parse(request.url);
+    switch (url_parts.pathname){
+
+        case '/':
+            var content = ejs.render(index_page, {
+                title: "Index",
+                content: "これはテンプレートエンジンを使ったサンプルページです。",
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+            break;
+        
+        case '/style.css':
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.write(style_css);
+            response.end();
+            break;
+
+        case '/images/anonymous.png':
+            response.writeHead(200, {'Content-Type': 'image/png'});
+            var photo01 = fs.readFileSync("images/anonymous.png","binary");
+            response.end(photo01,"binary");
+            break;
+
+        default:
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end('no page...');
+            break;
+    }
+}
